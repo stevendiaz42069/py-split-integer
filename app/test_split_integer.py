2c0c5f48d6e669670f36d33712507d8e09022f5e
steven42069 <stevendiaz152@gmail.com>
2025-11-23 19:52:01 -0500

    done now

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,15 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    parts = split_integer(8, 2)
-    assert max(parts) - min(parts) <= 1
-    assert sum(parts) == 8, "sum of parts must be equal to value"
+    assert sum(split_integer(8, 2)) == 8, "sum of parts must be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(8, 2)
-    assert len(parts) == 2
-    assert all(x == 8 // 2 for x in parts)
+    assert split_integer(8, 2) == [4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -18,9 +14,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    parts = split_integer(8, 3)
-    assert parts == sorted(parts)
+    assert split_integer(8, 3) == [2, 3, 3]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert 0 in split_integer(3, 4)
+    assert split_integer(3, 4) == [0, 1, 1, 1]
